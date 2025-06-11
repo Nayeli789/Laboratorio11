82aa5fcae1b629354f86bf6361aead129b1a443d
Nayeli789 <nsaavedralu@ucvvirtual.edu.pe>
2025-06-11 03:01:49 -0500

    Mensaje

--- examen1.py
+++ examen1.py
@@ -68,3 +68,13 @@ def ConsultarCartera():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+import os
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
